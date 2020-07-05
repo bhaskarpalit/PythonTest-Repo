e70360db9b84733ff425b12b710e93047555f68e
bhaskar.palit@aa.com <Bhaskar.Palit@aa.com>
2020-07-05 10:00:38 -0500

    Second commit

--- script.py
+++ script.py
@@ -4,10 +4,5 @@ import sys
 import requests
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://coreyms.com")
 print(r.status_code)
